22abad26b66d1a054c77f09016571e0e83838446
YunchuWang <yunchuwang5@gmail.com>
2021-03-25 23:42:52 -0500

    Remove auth

--- src/main/resources/user.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE User (
-    name varchar(64) NOT NULL COMMENT 'User name',
-    password varchar(1024) NOT NULL COMMENT 'Password',
-    user_role varchar(256)  NOT NULL COMMENT 'role',
-    PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
